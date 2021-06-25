a4d1c978c6693eb31c4176bf6eb5b8b41996f082
Jason <airjason13@gmail.com>
2021-06-25 09:20:00 +0800

    first commit

---
+++ global_def.py
@@ -0,0 +1,4 @@
+title = 'Web App'
+flask_server_port = 12345
+broadcast_port = 5000
+
---
+++ routes.py
@@ -0,0 +1,33 @@
+import os
+
+import cv2
+
+from main import app
+from flask import Flask, render_template, send_from_directory, request, redirect, url_for, Response, json
+from global_def import *
+
+@app.route("/")
+def index():
+
+    return render_template("index.html", title=title)
+
+def gen_video():
+
+    video = cv2.VideoCapture(0)
+    while True:
+        ret, frame = video.read()
+        if not ret:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+
+    return Response(gen_video(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    return
+
